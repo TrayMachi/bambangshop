154f5bac5f8d109458d7656989ef80d73ef322cb
TrayMachi <tristan.agra@ui.ac.id>
2025-03-28 19:09:21 +0700

    Implement subscribe function in Notification service

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -1,12 +1,22 @@
 use std::thread;
 
-use bambangshop::{compose_error_response, Result};
-use rocket::http::Status;
 use crate::model::notification::Notification;
 use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
 use crate::repository::subscriber::SubscriberRepository;
+use bambangshop::{compose_error_response, Result};
+use rocket::http::Status;
 
 pub struct NotificationService;
 
-impl NotificationService {}
+impl NotificationService {
+    pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscriber> {
+        let product_type_upper: String = product_type.to_uppercase();
+
+        let product_type_str: &str = product_type_upper.as_str();
+
+        let subscriber_result: Subscriber = SubscriberRepository::add(product_type_str, subscriber);
+
+        return Ok(subscriber_result);
+    }
+}
